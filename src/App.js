d85d7b4da3c587f230d84f118f1691260141ef7f
Jasim Uddin <jasimbdpro@gmail.com>
2024-05-08 10:31:20 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import FriendList from './components/FriendList/FriendList';
 import Header from './components/Header/Header';
 import Wrapper from './components/Wrapper/Wrapper';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Header></Header>
+      <Header></Header>
+      <div className='home-page'>
         <Wrapper></Wrapper>
+        <FriendList></FriendList>
       </div>
     </div>
   );
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FriendList = () => {
+    return (
+        <div>
+            <h2>Friend Added:</h2>
+            <p>Hello safsdalfdfkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkk</p>
+            <p>Hello safsdalfdfkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkk</p>
+            <p>Hello safsdalfdfkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkk</p>
+            <p>Hello safsdalfdfkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkk</p>
+            <p>Hello safsdalfdfkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkk</p>
+            <p>Hello safsdalfdfkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkk</p>
+            <p>Hello safsdalfdfkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkk</p>
+            <p>Hello safsdalfdfkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkkk</p>
+        </div>
+    );
+};
+
+export default FriendList;

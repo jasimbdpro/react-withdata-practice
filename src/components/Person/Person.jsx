3282549ceee0f4ba3d1b926774adfb433a233bbb
Jasim Uddin <jasimbdpro@gmail.com>
2024-12-06 17:34:37 +0600

    .

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../Person/Person.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
